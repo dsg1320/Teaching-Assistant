622b8324b72e949d624eae1e3e2937a3cbd6d6cc
dsg1320 <devika.sg.kpz@gmail.com>
2024-09-19 17:50:26 +0530

    Added Files

---
+++ backend/dist/db/connection.js
@@ -0,0 +1,22 @@
+import { connect } from "mongoose";
+import { disconnect } from "process";
+async function connecToDatabase() {
+    try {
+        await connect(process.env.MONGODB_URL);
+    }
+    catch (error) {
+        console.log(error);
+        throw new Error("Cannot connect to MongoDB");
+    }
+}
+async function disconnectFromDatabase() {
+    try {
+        await disconnect();
+    }
+    catch (error) {
+        console.log(error);
+        throw new Error("Cannot connect to MongoDB");
+    }
+}
+export { connecToDatabase, disconnectFromDatabase };
+//# sourceMappingURL=connection.js.map
---
+++ backend/dist/index.js
@@ -0,0 +1,8 @@
+import app from "./app.js";
+import { connecToDatabase } from "./db/connection.js";
+//connections and listeners
+const PORT = process.env.PORT || 5000;
+connecToDatabase().then(() => {
+    app.listen(5000, () => console.log("Server Open and Connected to database"));
+}).catch((err) => console.log(err));
+//# sourceMappingURL=index.js.map
