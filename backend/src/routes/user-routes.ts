899e91752248a087e6d3fe9e611104aa3b333579
dsg1320 <devika.sg.kpz@gmail.com>
2024-09-24 12:35:14 +0530

    Added changes

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { getAllUsers, userLogin, userSignup } from "../controllers/users-controllers.js";
+import { getAllUsers, userLogin, userSignup, verifyUser } from "../controllers/users-controllers.js";
 import { loginValidator, signupvalidator, validate } from "../utils/validators.js"
+import { verifyToken } from "../utils/token-manager.js";
 
 const useRoutes=Router();
 
 useRoutes.get("/",getAllUsers)
 useRoutes.post("/signup",validate(signupvalidator), userSignup);
 useRoutes.post("/login",validate(loginValidator), userLogin);
+useRoutes.get("/auth-status", verifyToken, verifyUser);
 
 export default useRoutes;
