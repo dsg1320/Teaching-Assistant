6a0697f1e25b5769af0643c8a836467ee033b488
Lekshmi <lekshmid2002@gmail.com>
2024-09-24 16:57:39 +0530

    added sessions

--- backend/dist/controllers/users-controllers.js
+++ backend/dist/controllers/users-controllers.js
@@ -1,9 +1,8 @@
-
 import User from "../models/User.js";
 import { hash, compare } from "bcrypt";
 import { createToken } from "../utils/token-manager.js";
 import { COOKIE_NAME } from "../utils/constants.js";
-import User from "../models/User.js";
+//import User from "../models/User.js";
 export const getAllUsers = async (req, res, next) => {
     //get all users
     try {
--- backend/dist/routes/chat-routes.js
+++ backend/dist/routes/chat-routes.js
@@ -3,5 +3,7 @@ import { handleChat } from '../controllers/chat-controller.js';
 const chatRoutes = Router();
 // Route to handle chat interactions
 chatRoutes.post('/chat', handleChat);
+// Route to handle chat interactions
+chatRoutes.post('/chat', handleChat);
 export default chatRoutes;
 //# sourceMappingURL=chat-routes.js.map
