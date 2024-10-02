c206185fb77e20bd880b31a2c76cf05be8c11db2
dsg1320 <devika.sg.kpz@gmail.com>
2024-10-02 15:07:28 +0530

    changed disconnect again

--- backend/dist/db/connection.js
+++ backend/dist/db/connection.js
@@ -1,5 +1,5 @@
 import { connect } from "mongoose";
-import { disconnect } from "process";
+import { disconnect } from "mongoose";
 async function connecToDatabase() {
     try {
         await connect(process.env.MONGODB_URL);
