ebf283cd17d78759b008114fead5de6093f1cf53
dsg1320 <73653978+dsg1320@users.noreply.github.com>
2024-10-09 02:43:24 +0530

    Update loginpage.dart

--- teaching-assistant-frontend/lib/pages/loginpage.dart
+++ teaching-assistant-frontend/lib/pages/loginpage.dart
@@ -22,7 +22,7 @@ class _LoginPageState extends State<LoginPage> {
 
     if (email.isNotEmpty && password.isNotEmpty) {
       String apiUrl =
-          "http://localhost:5001/api/v1/user/login"; // Your API URL for login
+          "https://teaching-assistant-production.up.railway.app/api/v1/user/login"; // Your API URL for login
 
       try {
         final response = await http.post(
